18caa53f9ff6ce41d49c2df78a4ea1add649ba71
Ayşe Güven <ayse.ylmz.78@gmail.com>
2021-06-03 22:52:35 +0300

    add project to sourcetree

---
+++ GitTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTestApp
+//
+//  Created by T41611 on 3.06.2021.
+//  Copyright © 2021 ayseyguven. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
